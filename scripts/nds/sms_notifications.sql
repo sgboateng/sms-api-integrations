0c73920f3c256f137837cb860957f1842ffac068
Samuel Boateng Gyebi <sgboateng@hotmail.com>
2026-03-07 19:15:36 +0000

    Modified MSSQL table objects in *dev-branch

--- scripts/nds/sms_notifications.sql
+++ scripts/nds/sms_notifications.sql
@@ -13,7 +13,7 @@ create table SMSNotifications
 (
 	Id              int not null identity(1,1),
   [Date]         	datetime not null default getdate(),
-  Response        text NULL,
+  Response        NVARCHAR(MAX),
   API             varchar(25) not null,
 
 constraint PKIdSMSNotifications primary key clustered(Id)
@@ -42,3 +42,8 @@ go
 create index SMSNotificationsAPI
   on SMSNotifications(API)
 go
+
+/*
+--SMS Notifications
+SELECT [Id], CONVERT(DATE, [Date]) [Date], JSON_VALUE([Response], '$.status') [Status], JSON_VALUE([Response], '$.code') [Code], JSON_VALUE([Response], '$.message') [Message], JSON_VALUE([Response], '$.summary._id') [_Id], JSON_VALUE([Response], '$.summary.message_id') [MessageId], JSON_VALUE([Response], '$.summary.type') [Type], JSON_VALUE([Response], '$.summary.total_sent') [TotalSent], JSON_VALUE([Response], '$.summary.contacts') [Contacts], JSON_VALUE([Response], '$.summary.total_rejected') [TotalRejected], JSON_VALUE([Response], '$.summary.numbers_sent[0]') [NumbersSent], JSON_VALUE([Response], '$.summary.credit_used') [CreditUsed], JSON_VALUE([Response], '$.summary.credit_left') [CreditLeft], JSON_VALUE([Response], '$.summary.wallet_used') [Wallet Used], [API] FROM [PracticeDB].[dbo].[SMSNotifications] 
+*/
